cb3db05ecc90648dbddd64634c9ef8ce5ebc7c4a
Allen He <allen.he@cyberbiz.io>
2023-09-29 16:23:09 +0800

    add graphic yo

--- src/contentScript.js
+++ src/contentScript.js
@@ -41,3 +41,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   sendResponse({});
   return true;
 });
+
+import { SVG } from '@svgdotjs/svg.js'
+
+const draw = SVG().addTo('body').size(300, 300)
+draw.rect(100, 100).attr({ fill: '#f06' })
