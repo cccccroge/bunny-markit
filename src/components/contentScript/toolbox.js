85b26d28a13b9fc1ed25c7e1f46666bdeff0a0da
Allen He <allen.he@cyberbiz.io>
2023-10-04 23:31:28 +0800

    wip ui

--- src/components/contentScript/toolbox.js
+++ src/components/contentScript/toolbox.js
@@ -1,9 +1,9 @@
 import { LitElement, html, css } from "lit";
 import { customElement, state } from "lit/decorators.js";
-// import '@shoelace-style/shoelace/dist/components/button/button.js'
 import '@shoelace-style/shoelace/dist/components/button-group/button-group.js'
 import '@shoelace-style/shoelace/dist/components/tab/tab.js'
 import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
+import '@shoelace-style/shoelace/dist/components/tooltip/tooltip.js';
 import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 
 @customElement('tool-box')
@@ -16,13 +16,30 @@ export class ToolBox extends LitElement {
       text-align: center;
       transform: translate(-50%, -50%);
     }
+    .tool-box-group {
+      background-color: var(--sl-color-neutral-50);
+      border-radius: var(--sl-border-radius-pill);
+      box-shadow: 0 2px 8px hsl(240 3.8% 46.1% / 50%);
+      padding: var(--sl-spacing-2x-small) var(--sl-spacing-small);
+    }
+    .button {
+      font-size: 2rem;
+      border-radius: var(--sl-border-radius-circle);
+    }
+    .button:hover {
+      background: var(--sl-color-neutral-200);
+    }
   `;
 
   render() {
     return html`
-      <sl-button-group label="Alignment">
-        <sl-icon-button name="app" style="font-size: 3rem">Text</sl-icon-button>
-        <sl-icon-button name="type" style="font-size: 3rem">Text</sl-icon-button>
+      <sl-button-group label="Alignment" class="tool-box-group">
+        <sl-tooltip content="Box(B)">
+          <sl-icon-button name="app" class="button">Box</sl-icon-button>
+        </sl-tooltip>
+        <sl-tooltip content="Text(T)">
+          <sl-icon-button name="type" class="button">Text</sl-icon-button>
+        </sl-tooltip>
       </sl-button-group>
     `;
   }
