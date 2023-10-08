95ab015c3fee77c137f620cc41273f2eeb25ec2f
Allen He <allen.he@cyberbiz.io>
2023-10-09 00:15:36 +0800

    remove hint part

--- src/contentScript/tools/EditTool.js
+++ src/contentScript/tools/EditTool.js
@@ -1,9 +1,9 @@
-import { SVG } from "@svgdotjs/svg.js";
 import Tool from "./Tool";
+import InputTool from "./InputTool";
 
 class EditTool extends Tool {
   enter() {
-    window.draw.css({ 'pointer-events': 'initial' })
+    window.draw.css({ 'pointer-events': 'initial', cursor: 'default' })
 
     this.onPointerOverCallback = this._onPointerOver.bind(this)
     this.onDblClickCallback = this._onDblClick.bind(this)
@@ -12,7 +12,7 @@ class EditTool extends Tool {
   }
 
   leave() {
-    window.draw.css({ 'pointer-events': 'none' })
+    window.draw.css({ 'pointer-events': 'none', cursor: 'initial' })
 
     document.removeEventListener("dblclick", this.onDblClickCallback)
     document.removeEventListener("pointerover", this.onPointerOverCallback)
@@ -27,37 +27,8 @@ class EditTool extends Tool {
 
     if (target.tagName === 'tspan') {
       // create editable element
-      const { x, y, width, height } = target.getBoundingClientRect()
-      const foreignContainer = document.createElementNS(
-        'http://www.w3.org/2000/svg',
-        'foreignObject'
-      )
-      foreignContainer.setAttribute('x', x)
-      foreignContainer.setAttribute('y', y)
-      foreignContainer.setAttribute('width', width * 10)
-      foreignContainer.setAttribute('height', height * 10)
-
-      const div = document.createElement('div')
-      div.setAttribute('contenteditable', 'true')
-      div.innerHTML = target.textContent
-      div.style="color: red; outline: none; font-size: 24px; font-family: 'Noto Sans', sans-serif;"
-
-      // replace the original
-      foreignContainer.appendChild(div)
-      const drawElement = window.draw.node
-      drawElement.appendChild(foreignContainer)
-
-      const originalText = SVG(target.parentElement)
-      originalText.css({ display: 'none' })
-
-      // start the edit
-      div.focus()
-      const range = document.createRange()
-      range.selectNodeContents(div)
-      range.collapse(false)
-      const selection = window.getSelection()
-      selection.removeAllRanges()
-      selection.addRange(range)
+      const inputTool = new InputTool(target)
+      inputTool.enter()
     }
   }
 }
--- src/contentScript/tools/TextTool.js
+++ src/contentScript/tools/TextTool.js
@@ -5,40 +5,23 @@ class TextTool extends Tool {
   constructor() {
     super()
     this.drawer = new TextDrawer(window.draw, {
-      defaultText: 'Text',
+      defaultText: 'Test',
       fontSize: 24,
       fontFamily: "'Noto Sans', sans-serif",
     })    
   }
 
   enter() {
-    if (document.body.style.cursor !== 'none') {
-      document.body.style.cursor = 'none'
+    if (document.body.style.cursor !== 'input') {
+      document.body.style.cursor = 'input'
     }
-    this.onPointOverCallback = this._onPointerOver.bind(this)
     this.onPointerDownCallback = this._onPointerDown.bind(this)
-    document.addEventListener("pointermove", this.onPointOverCallback)
     document.addEventListener("pointerdown", this.onPointerDownCallback)
   }
 
   leave() {
     document.body.style.cursor = ''
-    document.removeEventListener("pointermove", this.onPointOverCallback)
     document.removeEventListener("pointerdown", this.onPointerDownCallback)
-  }
-
-  // callback of showing floating text hint
-  _onPointerOver(event) {
-    this.drawer.eraseLast()
-
-    // ignore if over on toolboxs
-    const toolbox = document.querySelector('tool-box')
-    if (toolbox && toolbox.contains(event.target)) {
-      return
-    }
-
-    const { clientX, clientY } = event
-    this.drawer.drawHint({ x: clientX, y: clientY })
   }
 
   // callback of adding text to the drawer layer
