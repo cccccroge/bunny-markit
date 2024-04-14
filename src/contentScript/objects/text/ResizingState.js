5224d0066693a3c5a07c76d3074fcec175825179
Allen He <pooopooo543@gmail.com>
2024-04-14 14:31:56 +0800

    top left resizing

--- src/contentScript/objects/text/ResizingState.js
+++ src/contentScript/objects/text/ResizingState.js
@@ -39,14 +39,15 @@ export class ResizingState {
     const { x, y, control } = params
 
     this.control = control
-    this.originalPoint = {
-      x: this.svg.bbox().x,
-      y: this.svg.bbox().y,
+    this.initialTextCenterPosition = {
+      x: this.svg.cx(),
+      y: this.svg.cy(),
     }
     this.originalSize = {
       width: this.svg.bbox().width,
       height: this.svg.bbox().height,
     }
+    this.originalFontSize = this.svg.font()['font-size']
     this.startPoint = {
       x: x,
       y: y,
@@ -65,19 +66,21 @@ export class ResizingState {
 
     switch(this.control) {
       case 'topLeft': {
-        // this.svg.attr({
-        //   x: this.originalPoint.x - (this.startPoint.x - this.currentPoint.x),
-        //   y: this.originalPoint.y - (this.startPoint.y - this.currentPoint.y),
-        //   width: this.originalSize.width + (this.startPoint.x - this.currentPoint.x),
-        //   height: this.originalSize.height + (this.startPoint.y - this.currentPoint.y)
-        // })
-
+        // new font size = initial font size * (new diagonal length / initial diagonal length)
+        const initialDiagonalLength =
+          Math.sqrt(Math.pow(this.originalSize.width, 2) + Math.pow(this.originalSize.height, 2))
         const dx = this.startPoint.x - this.currentPoint.x
         const dy = this.startPoint.y - this.currentPoint.y
-        const delta = Math.pow(dx * dx + dy * dy, 0.5)
-        console.log('factor: ', 1 + delta / this.originalSize.width)
-        this.svg.scale(1 + delta / this.originalSize.width)
+        const delta_diagonal = Math.pow(dx * dx + dy * dy, 0.5) * (dx < 0 && dy < 0 ? -1 : 1)
+        const factor = (initialDiagonalLength + delta_diagonal) / initialDiagonalLength
+        this.svg.font({ size: this.originalFontSize * factor })
 
+        // move center point so that it looks like bottom right is fixed
+        // dx, dy need to be re-calculate since resizing is fixed ratio
+        const dx_actual = delta_diagonal * (this.originalSize.width / initialDiagonalLength)
+        const dy_actual = delta_diagonal * (this.originalSize.height / initialDiagonalLength)
+        this.svg.cx(this.initialTextCenterPosition.x - dx_actual / 2)
+        this.svg.cy(this.initialTextCenterPosition.y - dy_actual / 2)
         break
       }
       case 'top':
